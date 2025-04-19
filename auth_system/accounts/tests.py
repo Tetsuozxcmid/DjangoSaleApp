030fbd218f8c71ca984faedc0a1e2211e81e147e
tetsu <tetsuonakamura6@gmail.com>
2025-04-19 10:22:14 +0300

    added post_create tests

--- auth_system/accounts/tests.py
+++ auth_system/accounts/tests.py
@@ -5,12 +5,14 @@ from .models import Post, Offer
 
 # Create your tests here.
 
+
 class UserAuthTests(TestCase):
     def setUp(self):
         self.client = Client()
         self.register_url = reverse('auth_register')
         self.login_url = reverse('auth_login')
-        self.user = User.objects.create_user(username='testuser', password='pass1234')
+        self.user = User.objects.create_user(
+            username='testuser', password='pass1234')
 
     def test_register_user(self):
         response = self.client.post(self.register_url, {
@@ -22,9 +24,38 @@ class UserAuthTests(TestCase):
         self.assertTrue(User.objects.filter(username='newuser').exists())
 
     def test_login_user(self):
-        
+
         response = self.client.post(self.login_url, {
             'username': 'testuser',
             'password': 'pass1234'
         })
         self.assertEqual(response.status_code, 302)
+
+
+class PostCreatesTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.post_create_url = reverse('create_post')
+        self.user = User.objects.create_user(
+            username='testpostuser', password='pass12345')
+
+    def test_create_post(self):
+
+        self.client.login(username='testpostuser', password='pass12345')
+
+        response = self.client.post(self.post_create_url, {
+            'title': 'Тестовое обьявление',
+            'description': 'Описание для теста',
+            'category': 'clothing',
+            'condition': "new",
+        })
+
+        self.assertEqual(response.status_code, 302)
+        if response.status_code == 200:
+            print("Form errors:", response.context['form'].errors)
+        from .models import Post
+        self.assertTrue(Post.objects.filter(
+            title='Тестовое обьявление').exists())
+
+        post = Post.objects.get(title='Тестовое обьявление')
+        self.assertEqual(post.author, self.user)
